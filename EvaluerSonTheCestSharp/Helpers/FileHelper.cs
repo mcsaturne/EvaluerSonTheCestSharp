eaaf15e4be44a94f58d17199f5f309a1889fade2
Maxime Grenier <mc.saturne@gmail.com>
2017-03-03 23:13:20 -0500

    Modifications + VS files
    Removed the ref on method. Added a FileHelper.

---
+++ EvaluerSonTheCestSharp/Helpers/FileHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace EvaluerSonTheCestSharp.Helpers
+{
+    class FileHelper
+    {
+        public static void lireDansLaBD(List<The> listeDesThes, string path)
+        {
+            try
+            {
+                using (var file = new StreamReader(path))
+                {
+                    string ligne;
+                    while ((ligne = file.ReadLine()) != null)
+                    {
+                        var the = new The();
+                        IEnumerable<string> ligneThe = new List<string>();
+                        ligneThe = ligne.Split('~');
+                        the.nom = ligneThe.ElementAt(0);
+                        the.saveurs = ligneThe.ElementAt(1).Split('=').ToList<string>();
+                        the.description = ligneThe.ElementAt(2);
+                        the.magasin = ligneThe.ElementAt(3);
+                        the.note = Int32.Parse(ligneThe.ElementAt(4));
+                        listeDesThes.Add(the);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Le fichier n'existe pas");
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public static void ecrireDansLaBD(List<The> listeDesThes, string path)
+        {
+            try
+            {
+                using (var fs = new FileStream(path, FileMode.Create))
+                {
+                    using (var writer = new StreamWriter(fs, Encoding.Default))
+                    {
+                        foreach (var i in listeDesThes)
+                        {
+                            writer.WriteLine(i.objetVersFichier());
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erreur dans l'ecriture du fichier");
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
--- EvaluerSonTheCestSharp/Program.cs
+++ EvaluerSonTheCestSharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EvaluerSonTheCestSharp.Helpers;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,8 +13,9 @@ namespace EvaluerSonTheCestSharp
         {
             The the = new The();
             int choix;
+            string path = $@"{AppDomain.CurrentDomain.BaseDirectory}\\petiteBD.txt";
             List<The> listeDesThes = new List<The>();
-            lireDansLaBD(ref listeDesThes);
+            FileHelper.lireDansLaBD(listeDesThes, path);
             Console.WriteLine("Debut du programme");
 
             do
@@ -22,7 +24,7 @@ namespace EvaluerSonTheCestSharp
                 switch (choix)
                 {
                     case 1:
-                        the = ajouterUnThe(ref listeDesThes);
+                        the = ajouterUnThe(listeDesThes);
                         if (the != null)
                         {
                             listeDesThes.Add(the);
@@ -33,23 +35,24 @@ namespace EvaluerSonTheCestSharp
                         Console.Write("Entrez le nom du the a modifier : ");
                         string nomDuTheAModifier = Console.ReadLine();
                         The resThe = listeDesThes.Find(e => e.nom == nomDuTheAModifier);
-                        modifierTheExistant(listeDesThes, ref resThe);
+                        modifierTheExistant(listeDesThes, resThe);
                         break;
                     case 3:
-                        supprimerThe(ref listeDesThes);
+                        supprimerThe(listeDesThes);
                         break;
                     case 4:
-                        trierLesThes(ref listeDesThes);
+                        trierLesThes(listeDesThes);
                         break;
                     case 5:
-                        ecrireDansLaBD(ref listeDesThes);
+                        FileHelper.ecrireDansLaBD(listeDesThes, path);
                         Console.WriteLine("Fin du programme");
                         break;
                 }
             } while (choix != 5);
         }
 
-        private static void trierLesThes(ref List<The> listeDesThes) {
+        private static void trierLesThes(List<The> listeDesThes)
+        {
             Console.WriteLine("Trier les thes par...");
             Console.WriteLine("1 - Note");
             Console.WriteLine("2 - Nom");
@@ -57,7 +60,8 @@ namespace EvaluerSonTheCestSharp
             Console.Write("Votre choix : ");
             string choix = Console.ReadLine();
             List<The> listeTriee = new List<The>();
-            switch (choix) {
+            switch (choix)
+            {
                 case "1":
                     listeTriee = listeDesThes.OrderBy(o => o.nom).ToList<The>();
                     listeTriee.ForEach(o => Console.WriteLine(o.ToString()));
@@ -75,7 +79,8 @@ namespace EvaluerSonTheCestSharp
 
         }
 
-        private static void supprimerThe(ref List<The> listeDesThes) {
+        private static void supprimerThe(List<The> listeDesThes)
+        {
             Console.Write("Entrez le nom du the a supprimer : ");
             string nomDuTheASupprimer = Console.ReadLine();
             The resThe = listeDesThes.Find(e => e.nom == nomDuTheASupprimer);
@@ -86,61 +91,75 @@ namespace EvaluerSonTheCestSharp
 
         }
 
-        static void modifierTheExistant(List<The> listeDesThes, ref The resThe) {
-            
+        static void modifierTheExistant(List<The> listeDesThes, The resThe)
+        {
+
             if (resThe != null)
             {
                 int choix = AfficherMenuModifier();
-                
-                switch (choix){
-                    case 1: changerNomThe(ref resThe);
-                              break;
-                    case 2: ajouterUneSaveur(ref resThe);
-                              break;
-                    case 3: changerLaDescription(ref resThe);
-                              break;
-                    case 4: changerLeMagasin(ref resThe);
-                              break;
-                    case 5: changerLaNote(ref resThe);
-                              break;
+
+                switch (choix)
+                {
+                    case 1:
+                        changerNomThe(resThe);
+                        break;
+                    case 2:
+                        ajouterUneSaveur(resThe);
+                        break;
+                    case 3:
+                        changerLaDescription(resThe);
+                        break;
+                    case 4:
+                        changerLeMagasin(resThe);
+                        break;
+                    case 5:
+                        changerLaNote(resThe);
+                        break;
                 }
 
                 Console.WriteLine("\t Modification du the reussi\n" + resThe.ToString());
             }
-            else {
+            else
+            {
                 Console.WriteLine("Le the n'existe pas");
             }
         }
 
-        private static void changerLaNote(ref The resThe) {
+        private static void changerLaNote(The resThe)
+        {
             Console.Write("Entrez une nouvelle note : ");
             string note = Console.ReadLine();
             resThe.note = Int32.Parse(note);
         }
 
-        private static void changerLeMagasin(ref The resThe) {
+        private static void changerLeMagasin(The resThe)
+        {
             Console.Write("Entrez un nouveau magasin : ");
             string magasin = Console.ReadLine();
             resThe.magasin = magasin;
         }
 
-        private static void changerLaDescription(ref The resThe) {
+        private static void changerLaDescription(The resThe)
+        {
             Console.Write("Entrez une nouvelle description : ");
             string description = Console.ReadLine();
             resThe.description = description;
         }
 
-        private static void changerNomThe(ref The resThe) {
+        private static void changerNomThe(The resThe)
+        {
             Console.Write("Entrez le nouveau nom du the : ");
             string nom = Console.ReadLine();
             resThe.nom = nom;
         }
 
-        private static void ajouterUneSaveur(ref The resThe) {
+        private static void ajouterUneSaveur(The resThe)
+        {
             Console.Write("Entrez des saveurs au the : ");
             string saveurs = Console.ReadLine();
             string[] tabSaveurs = saveurs.Split(' ');
-            foreach (var i in tabSaveurs) {
+            foreach (var i in tabSaveurs)
+            {
                 resThe.saveurs.Add(i);
             }
         }
@@ -161,13 +180,15 @@ namespace EvaluerSonTheCestSharp
             return nombre;
         }
 
-        public static The ajouterUnThe(ref List<The> listeDesThes) {
+        public static The ajouterUnThe(List<The> listeDesThes)
+        {
             The theCree = new The();
-            theCree = obtenirInfoThe(ref listeDesThes);
+            theCree = obtenirInfoThe(listeDesThes);
             return theCree;
         }
 
-        static The obtenirInfoThe(ref List<The> listeDesThes) {
+        static The obtenirInfoThe(List<The> listeDesThes)
+        {
             The theACreer = new The();
             Console.Write("Entrez le nom du thé : ");
             theACreer.nom = Console.ReadLine();
@@ -188,19 +209,22 @@ namespace EvaluerSonTheCestSharp
                 Console.Write("Entrez une note : ");
                 theACreer.note = Int32.Parse(Console.ReadLine());
             }
-            else {
+            else
+            {
                 theACreer = null;
                 Console.WriteLine("Le the existe deja. Voici les informations");
                 Console.WriteLine(resThe.ToString());
                 Console.Write("Souhaitez-vous le modifier ? (O/N) : ");
                 string choix = Console.ReadLine();
-                if (choix == "O") {
-                    modifierTheExistant(listeDesThes, ref resThe);
+                if (choix == "O")
+                {
+                    modifierTheExistant(listeDesThes, resThe);
                 }
             }
             return theACreer;
 
         }
+
         public static int afficherMenu()
         {
             string choix;
@@ -216,66 +240,8 @@ namespace EvaluerSonTheCestSharp
                 Console.Write("Votre choix : ");
                 choix = Console.ReadLine();
                 Int32.TryParse(choix, out nombre);
-            } while (nombre < 0 || nombre > 6 );
+            } while (nombre < 0 || nombre > 6);
             return nombre;
-        }
-
-        private static void lireDansLaBD(ref List<The> listeDesThes) {
-            string ligne;
-            The the = null;
-            StreamReader file = null;
-            try
-            {
-                file = new StreamReader(@"C:\Users\Maxime Grenier\Documents\petiteBD.txt");
-                while ((ligne = file.ReadLine()) != null)
-                {
-                    the = new The();
-                    IEnumerable<string> ligneThe = new List<string>();
-                    ligneThe = ligne.Split('~');
-                    the.nom = ligneThe.ElementAt(0);
-                    the.saveurs = ligneThe.ElementAt(1).Split('=').ToList<string>();
-                    the.description = ligneThe.ElementAt(2);
-                    the.magasin = ligneThe.ElementAt(3);
-                    the.note = Int32.Parse(ligneThe.ElementAt(4));
-                    listeDesThes.Add(the);
-                }
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("Le fichier n'existe pas");
-                Console.WriteLine(e.ToString());
-            }
-            finally {
-                if (file != null)
-                    file.Dispose();
-            }
-            
-        }
-
-        private static void ecrireDansLaBD(ref List<The> listeDesThes) {
-            string chemin = "C:\\Users\\Maxime Grenier\\Documents\\petiteBD.txt";
-            FileStream fs = null;
-            try
-            {
-                fs = new FileStream(chemin, FileMode.Create);
-                using (StreamWriter writer = new StreamWriter(fs, Encoding.Default))
-                {
-                    foreach (var i in listeDesThes)
-                    {
-                        writer.WriteLine(i.objetVersFichier());
-                    }
-                }
-
-            }
-            catch (Exception e) {
-                Console.WriteLine("Erreur dans l'ecriture du fichier");
-                Console.WriteLine(e.ToString());
-            }
-            finally
-            {
-                if (fs != null)
-                    fs.Dispose();
-            }
         }
     }
 }
